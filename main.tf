61dd76f3c3bfbb58f4c1e3cab412b45775c0dc3b
John C. Bland II <johncblandii@gmail.com>
2020-03-02 09:35:08 -0600

    chore: passing TF

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 resource "aws_security_group_rule" "my-rule" {
     type = "ingress"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS006
     description = "my rule"
 }
 
 resource "aws_alb_listener" "my-alb-listener" {
-    port     = "80"
-    protocol = "HTTP"
+    port     = "443"
+    protocol = "HTTPS"
 }
 
-resource "aws_db_security_group" "my-group" {
-}
+# resource "aws_db_security_group" "my-group" {
+# }
 
 resource "azurerm_managed_disk" "source" {
     encryption_settings {
-        enabled = false
+        enabled = true
     }
 }
